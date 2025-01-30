100153256677cab721a1277b990c31aa8cdb56dc
Azuar <azuarok@gmail.com>
2025-01-31 04:46:52 +0700

    update skripsi model

--- src/models/skripsi.model.js
+++ src/models/skripsi.model.js
@@ -7,12 +7,18 @@ const SkripsiSchema = new Schema(
         judul: { type: String, required: true },
         pembimbing1: {type: Object, required: true},
         pembimbing2: {type: Object, required: true},
+        penguji1: {type: Object, required: false},
+        penguji2: {type: Object, required: false},
+        penguji3: {type: Object, required: false},
         setuju1: {type: Boolean, required: false},
         setuju2: {type: Boolean, required: false},
         status: {type: String, required: false},
         waktuSeminar: {type: String, required: false},
         waktuSidang: {type: String, required: false},
         mahasiswa: {type:Object, required: false},
+        drive: {type:String, required: false},
+        tanggal_seminar: {type:String, required: false},
+        waktu_seminar: {type:String, required: false},
     },
     {
         timestamps: {
